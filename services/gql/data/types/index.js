8bc175696d483398887b9601dec90f0dfae57051
Łukasz Jagodziński <luke.jagodzinski@gmail.com>
2018-05-08 21:21:39 +0200

    Remove old way of defining schema

--- services/gql/data/types/index.js
+++ services/gql/data/types/index.js
@@ -6,28 +6,3 @@ import combineAstSchemas from "../../utils/combineAstSchemas";
 import schema from "./schema.graphql";
 
 export default combineAstSchemas([Mutation, Query, User, UserInput, schema]);
-
-// export default `
-// input UserInput {
-//   email: String
-//   password: String
-//   firstName: String
-//   lastName: String
-// }
-// type User {
-//   id: ID!
-//   email: String
-//   firstName: String
-//   lastName: String
-// }
-// type Query {
-//   user(id: String!): User
-// }
-// type Mutation {
-//   createUser(input: UserInput): User
-// }
-// schema {
-//   mutation: Mutation
-//   query: Query
-// }
-// `;
